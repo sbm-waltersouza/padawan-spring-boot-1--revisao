a5ac2647e8529ec39ed45ec1de164180cb7c2186
sbm-waltersouza <169094760+sbm-waltersouza@users.noreply.github.com>
2024-07-30 16:37:51 -0300

    tudo aqui é backend java spring boot

---
+++ src/main/java/com/example/backend/VideoService.java
@@ -0,0 +1,18 @@
+package com.example.backend;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoService {
+    @Autowired
+    private VideoRepository videoRepository;
+
+    public List<Video> getAllVideos() {
+        return videoRepository.findAll();
+    }
+
+    
+}
